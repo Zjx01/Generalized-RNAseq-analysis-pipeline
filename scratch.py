86ae6f079eebeb73a1df37310b1ad1af966f6f4b
Zjx01 <jesizhao@umich.edu>
2023-02-08 23:48:18 -0500

    add dist folder

--- scratch.py
+++ scratch.py
@@ -11,15 +11,22 @@ def read_images(img_dir):
 
 def check_img_qc(img,thresh):
     #to see if the image has a clear forground or background
-    # can adopt adaptive threshold calculating
+    # can adopt adaptive threshold calculating and compute the activity score
     if img.mean() > thresh:
+        #should check the foreground and background ratio of the image 
+        #compute the activity score
         return True
+
     else:
         return False
+
+def generate_mask(img):
+    pass
+
 
 def validate_segment(img):
     pass
 
     
         
-    
+#update code: remember to 
