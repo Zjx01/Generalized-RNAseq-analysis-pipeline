5ff80708deb641bde6d74110786866249e5f659f
Zjx01 <48267562+Zjx01@users.noreply.github.com>
2023-02-22 19:28:28 -0500

    Add files via upload
    update test case

--- test/test_sra_to_fastq.py
+++ test/test_sra_to_fastq.py
@@ -11,13 +11,16 @@ def test_sra_to_fastq():
     expected = 'sra file to fastq file completed'
     assert test_result == expected, f'The sra is successfully converted to fastq'
 
+
+
 def test_edge_case():
     #edge cases
     edge_sra_dir= '/path_unexist'
     paired = False
     edge_expected = 'the input SRA path did not exist'
-    with pytest.raises(AttributeError):
-        a = prepc.sra_to_fastq(sra_dir = edge_sra_dir,paired = paired)
-        assert a.type is AttributeError
+    test_result = prepc.sra_to_fastq(sra_dir = edge_sra_dir,paired = paired)
+    assert test_result == edge_expected, f'the edge cases of wrong input file path'
+
+
 
 
