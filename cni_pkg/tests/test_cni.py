06fd684a4840517df66d18a68caebc5650d5015f
Zjx01 <48267562+Zjx01@users.noreply.github.com>
2023-02-22 19:17:58 -0500

    Delete cni_pkg directory

--- cni_pkg/tests/test_cni.py
+++
@@ -1,8 +0,0 @@
-from cni_pkg import scratch as sc 
-import numpy as np
-def test_check_img_qc():
-	img = np.array(range(100,135))
-	thresh = 120
-	expected = True
-	test_Qc = sc.check_img_qc(img,thresh) 
-	assert test_Qc == expected, f"The image passed through the test"
